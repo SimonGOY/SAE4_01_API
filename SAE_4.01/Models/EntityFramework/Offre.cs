dc50d54f774b7ece64c5467fa6d3d1e8e70f056c
dahigere <dahigere@D361-S30>
2024-03-12 15:42:28 +0100

    table

--- SAE_4.01/Models/EntityFramework/Offre.cs
+++ SAE_4.01/Models/EntityFramework/Offre.cs
@@ -1,6 +1,20 @@
-﻿namespace SAE_4._01.Models.EntityFramework
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace SAE_4._01.Models.EntityFramework
 {
+    [Table("t_e_offre_ofr")]
     public class Offre
     {
+        [Key]
+        [Column("ofr_id")]
+        public int IdOffre { get; set; }
+
+        [Column("ofr_assurance")]
+        public bool Assurance { get; set; }
+
+        [Column("ofr_financement")]
+        [StringLength(30)]
+        public string? FinancementOffre { get; set; } = null!;
     }
 }
---
+++ SAE_4.01/Models/EntityFramework/Telephone.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SAE_4._01.Models.EntityFramework
+{
+    public class Telephone
+    {
+    }
+}
