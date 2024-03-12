92f8a6cbeea27f262a710fff411d774b799a15a1
SIMON GOY <sg.bonneville@gmail.com>
2024-03-12 17:39:59 +0100

    Création table pack

---
+++ SAE_4.01/Models/EntityFramework/Pack.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SAE_4._01.Models.EntityFramework
+{
+    [Table("t_e_pack_pck")]
+    public class Pack
+    {
+        [Key]
+        [Column("pck_id")]
+        public int IdPack { get; set; }
+        // A faire :  la foreign key
+        [Column("mod_id")]
+        public int IdMoto { get; set; }
+        [Column("pck_nom")]
+        [StringLength(20)]
+        public string NomPack { get; set; } = null!;
+        [Column("pck_description")]
+        public string DescriptionPack { get; set; } = null!;
+        [Column("pck_photo")]
+        [StringLength(50)]
+        public string PhotoPack { get; set; } = null!;
+        [Column("pck_prix", TypeName = "DECIMAL(6,2)")]
+        public decimal PrixPack { get; set; }
+    }
+}
