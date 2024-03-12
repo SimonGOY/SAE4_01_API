3c46d1e07e796191a2281d048243ea34e06f417c
SIMON GOY <sg.bonneville@gmail.com>
2024-03-12 17:43:33 +0100

    Création table se compose

---
+++ SAE_4.01/Models/EntityFramework/SeCompose.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SAE_4._01.Models.EntityFramework
+{
+    [Table("t_j_se_compose_sec")]
+    public class SeCompose
+    {
+        [Key]
+        [Column("pck_id")]
+        public int IdPack { get; set; }
+        [Key]
+        [Column("opt_id")]
+        public int IdOption { get; set; }
+
+        // A faire les 2 foreing keys
+    }
+}
