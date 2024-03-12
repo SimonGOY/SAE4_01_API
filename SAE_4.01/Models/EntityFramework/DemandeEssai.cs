095225cf8743a25ed5140dc16fcc3996e5ba235f
DatSword <qbastard99@gmail.com>
2024-03-12 15:54:33 +0100

    DemandeEssai

--- SAE_4.01/Models/EntityFramework/DemandeEssai.cs
+++ SAE_4.01/Models/EntityFramework/DemandeEssai.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Drawing;
 
 namespace SAE_4._01.Models.EntityFramework
 {
@@ -21,5 +22,17 @@ namespace SAE_4._01.Models.EntityFramework
 
         [Column("dme_descriptif")]
         public string? DescriptifDemandeEssai { get; set; }
+
+        [ForeignKey(nameof(IdConcessionnaire))]
+        [InverseProperty(nameof(Concessionnaire.DemandeEssaiConcessionnaire))]
+        public virtual Concessionnaire ConcessionnaireDemandeEssai { get; set; } = null!;
+
+        [ForeignKey(nameof(IdMoto))]
+        [InverseProperty(nameof(ModelMoto.DemandeEssaiModelMoto))]
+        public virtual ModelMoto ModelMotoDemandeEssai { get; set; } = null!;
+
+        [ForeignKey(nameof(IdContact))]
+        [InverseProperty(nameof(ContactInfo.DemandeEssaiContactInfo))]
+        public virtual ContactInfo ContactInfoDemandeEssai { get; set; } = null!;
     }
 }
