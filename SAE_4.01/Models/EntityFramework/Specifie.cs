e322b3caf01f6bfed741c7e3f1d1c7ea4d5f0136
SIMON GOY <sg.bonneville@gmail.com>
2024-03-12 17:27:54 +0100

    Création table spécifie

---
+++ SAE_4.01/Models/EntityFramework/Specifie.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SAE_4._01.Models.EntityFramework
+{
+    [Table("t_j_specifie_spe")]
+    public class Specifie
+    {
+        [Key]
+        [Column("spe_idmoto")]
+        public int IdMoto { get; set; }
+        [Key]
+        [Column("opt_id")]
+        public int IdOption { get; set;}
+        [Column("mcf_id")]
+        public int IdMotoConfigurable { get; set;}
+
+        /* A faire les 2 fk idmcf et idopt */
+    }
+}
