004757830da428db5760e511f7d42d00f11960e0
DatSword <qbastard99@gmail.com>
2024-03-12 15:12:21 +0100

    Stock

--- SAE_4.01/Models/EntityFramework/Stock.cs
+++ SAE_4.01/Models/EntityFramework/Stock.cs
@@ -23,5 +23,17 @@ namespace SAE_4._01.Models.EntityFramework
 
         [Column("stk_quantite")]
         public int Quantite { get; set; }
+
+        [ForeignKey(nameof(IdTaille))]
+        [InverseProperty(nameof(Taille.StockTaille))]
+        public virtual Taille TailleStock { get; set; } = null!;
+
+        [ForeignKey(nameof(IdColoris))]
+        [InverseProperty(nameof(Coloris.StockColoris))]
+        public virtual Coloris ColorisStock { get; set; } = null!;
+
+        [ForeignKey(nameof(IdEquipement))]
+        [InverseProperty(nameof(Equipement.StockEquipement))]
+        public virtual Equipement EquipementStock { get; set; } = null!;
     }
 }
