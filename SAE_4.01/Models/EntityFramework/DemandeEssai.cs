d4b272976646e7c17452765f217235b5ba18b61e
DatSword <qbastard99@gmail.com>
2024-03-12 15:44:41 +0100

    Demande Essai

---
+++ SAE_4.01/Models/EntityFramework/DemandeEssai.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SAE_4._01.Models.EntityFramework
+{
+    [Table("t_e_demandeessai_dme")]
+    public class DemandeEssai
+    {
+        [Key]
+        [Column("dme_id")]
+        public int IdDemandeEssai { get; set; }
+
+        [Column("con_id")]
+        public int IdConcessionnaire { get; set; }
+
+        [Column("mom_id")]
+        public int IdMoto { get; set; }
+
+        [Column("ctf_id")]
+        public int IdContact { get; set; }
+
+        [Column("dme_descriptif")]
+        public string? DescriptifDemandeEssai { get; set; }
+    }
+}
