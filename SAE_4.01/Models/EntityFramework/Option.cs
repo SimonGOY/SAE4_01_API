4fc74f979d46e5bffe6a5bca8bdb0ff5caab0229
SIMON GOY <sg.bonneville@gmail.com>
2024-03-12 17:32:38 +0100

    Création table Option

---
+++ SAE_4.01/Models/EntityFramework/Option.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SAE_4._01.Models.EntityFramework
+{
+    [Table("t_e_option_opt")]
+    public class Option
+    {
+        [Key]
+        [Column("opt_id")]
+        public int IdOption { get; set; }
+        [Column("opt_nom")]
+        [StringLength(50)]
+        public string NomOption { get; set; } = null!;
+        [Column("opt_prix")]
+        public decimal PrixOption { get; set; }
+        [Column("opt_detail")]
+        public string DetailOption { get; set; } = null!;
+        [Column("opt_photo")]
+        public string PhotoOption { get; set; } = null!;
+    }
+}
