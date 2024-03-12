98d3d5012739e6faa6e826559f9385c07962b5e8
dahigere <dahigere@D361-S30>
2024-03-12 15:57:06 +0100

    table

--- SAE_4.01/Models/EntityFramework/Telephone.cs
+++ SAE_4.01/Models/EntityFramework/Telephone.cs
@@ -3,7 +3,23 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace SAE_4._01.Models.EntityFramework
 {
+    [Table("t_e_telephone_tel")]
     public class Telephone
     {
+        [Key]
+        [Column("tel_id")]
+        public int Id { get; set; }
+
+        [Column("tel_num")]
+        [StringLength(20)]
+        public string? NumTelephone { get; set; }
+
+        [Column("tel_type")]
+        [StringLength(60)]
+        public string? Type { get; set; } = null!;
+
+        [Column("tel_fonction")]
+        [StringLength(25)]
+        public string? Fonction { get; set; } = null!;
     }
 }
