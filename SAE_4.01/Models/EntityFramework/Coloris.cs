8fc1d8b9d5bb6e9adf393c0a3ddcb7ae891afb2e
dahigere <dahigere@D361-S30>
2024-03-12 17:02:14 +0100

    table

---
+++ SAE_4.01/Models/EntityFramework/Coloris.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SAE_4._01.Models.EntityFramework
+{
+    [Table("t_e_coloris_cls")]
+    public class Coloris
+    {
+        [Key]
+        [Column("cls_id")]
+        public int IdColoris { get; set; }
+
+        [Column("cls_nom")]
+        [StringLength(20)]
+        public string? NomColoris { get; set; } = null!;
+    }
+}
