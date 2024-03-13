9207b19291146e9f38e6047c277ab46cc98eb220
dahigere <dahigere@D361-S30>
2024-03-13 10:16:55 +0100

    Controleur a la rache

---
+++ SAE_4.01/Controllers/ClientsController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SAE_4._01.Models.EntityFramework;
+
+namespace SAE_4._01.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientsController : ControllerBase
+    {
+        private readonly BMWDBContext _context;
+
+        public ClientsController(BMWDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Clients
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Client>>> GetClients()
+        {
+            if (_context.Clients == null)
+            {
+                return NotFound();
+            }
+            return await _context.Clients.ToListAsync();
+        }
+
+        // GET: api/Clients/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Client>> GetClient(int id)
+        {
+            if (_context.Clients == null)
+            {
+                return NotFound();
+            }
+            var Client = await _context.Clients.FindAsync(id);
+
+            if (Client == null)
+            {
+                return NotFound();
+            }
+
+            return Client;
+        }
+
+        // PUT: api/Clients/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutClient(int id, Client Client)
+        {
+            if (id != Client.IdClient)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(Client).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ClientExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Clients
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Client>> PostClient(Client Client)
+        {
+            if (_context.Clients == null)
+            {
+                return Problem("Entity set 'FilmRatingsDBContext.Clients'  is null.");
+            }
+            _context.Clients.Add(Client);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetClient", new { id = Client.IdClient }, Client);
+        }
+
+        // DELETE: api/Clients/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteClient(int id)
+        {
+            if (_context.Clients == null)
+            {
+                return NotFound();
+            }
+            var Client = await _context.Clients.FindAsync(id);
+            if (Client == null)
+            {
+                return NotFound();
+            }
+
+            _context.Clients.Remove(Client);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ClientExists(int id)
+        {
+            return (_context.Clients?.Any(e => e.IdClient == id)).GetValueOrDefault();
+        }
+    }
+}
