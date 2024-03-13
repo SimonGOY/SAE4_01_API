3826679802efdb367d3f0e19b12cf4e7dbba36d8
SIMON GOY <sg.bonneville@gmail.com>
2024-03-13 09:46:10 +0100

    Création table Paramètre

---
+++ SAE_4.01/Models/EntityFramework/Parametres.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SAE_4._01.Models.EntityFramework
+{
+    [Table("t_e_parametre_par")]
+    public class Parametres
+    {
+        [Key]
+        [Column("par_nom")]
+        [StringLength(50)]
+        public string NomParametre { get; set; } = null!;
+        [Column("par_description")]
+        [StringLength(50)]
+        public string Description { get; set;} = null!;
+    }
+}
