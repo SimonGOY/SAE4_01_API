0423a869c65a2f3a8cee204bbde43741f004b625
DatSword <qbastard99@gmail.com>
2024-03-19 17:36:29 +0100

    Si c'est pas la dernière correction je meurs

--- SAE_4.01/Models/EntityFramework/Users.cs
+++ SAE_4.01/Models/EntityFramework/Users.cs
@@ -10,8 +10,9 @@ namespace SAE_4._01.Models.EntityFramework
         [Column("usr_id")]
         public int Id { get; set; }
 
-        [Column("clt_id")]
-        public int IdClient { get; set; }
+        [Column("usr_firstname")]
+        [StringLength(255)]
+        public string FirstName { get; set; } = null!;
 
         [Column("usr_email")]
         [StringLength(255)]
@@ -20,17 +21,28 @@ namespace SAE_4._01.Models.EntityFramework
         [Column("usr_password")]
         public string Password { get; set; } = null!;
 
-        [Column("usr_remembertoken")]
-        public string? RememberToken { get; set; }
-
         [Column("usr_createdat", TypeName = "DATE")]
         public DateTime CreatedAt { get; set; }
 
         [Column("usr_updatedat", TypeName = "DATE")]
         public DateTime UpdatedAt { get; set; }
 
+        [Column("usr_civilite")]
+        [StringLength(10)]
+        public string Civilite { get; set; } = null!;
+
+        [Column("usr_lastname")]
+        [StringLength(255)]
+        public string LastName { get; set; } = null!;
+
+        [Column("clt_id")]
+        public int IdClient { get; set; }
+
         [Column("usr_iscomplete")]
         public bool IsComplete { get; set; }
+
+        [Column("usr_type")]
+        public int TypeCompte { get; set; }
 
         [Column("usr_doubleauth")]
         public bool DoubleAuth { get; set; }
