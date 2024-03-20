96188745c28687ac2124daa6c0fd6408f2e9fe54
DatSword <qbastard99@gmail.com>
2024-03-20 15:06:55 +0100

    data manager categorie equipement manager

--- SAE_4.01/Program.cs
+++ SAE_4.01/Program.cs
@@ -27,6 +27,7 @@ namespace SAE_4._01
             builder.Services.AddScoped<IDataRepository<Caracteristique>, CaracteristiqueManager>();
             builder.Services.AddScoped<IDataRepository<CategorieAccessoire>, CategorieAccessoireManager>();
             builder.Services.AddScoped<IDataRepository<CategorieCaracteristique>, CategorieCaracteristiqueManager>();
+            builder.Services.AddScoped<IDataRepository<CategorieEquipement>, CategorieEquipementManager>();
 
             builder.Services.AddScoped<IDataRepository<Users>, UserManager>();
 
