d639fef6b3130c83610d8f5e62489dca36881b29
DatSword <qbastard99@gmail.com>
2024-03-20 16:58:52 +0100

    datamanager garage

---
+++ SAE_4.01/Models/DataManager/GarageManager.cs
@@ -0,0 +1,50 @@
+﻿using SAE_4._01.Models.EntityFramework;
+using SAE_4._01.Models.Repository;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Runtime.ConstrainedExecution;
+
+namespace SAE_4._01.Models.DataManager
+{
+    public class GarageManager : IDataRepository<Garage>
+    {
+        readonly BMWDBContext _dbContext;
+
+        public GarageManager() { }
+
+        public GarageManager(BMWDBContext context)
+        {
+            _dbContext = context;
+        }
+
+        public async Task<ActionResult<IEnumerable<Garage>>> GetAllAsync()
+        {
+            return await _dbContext.Garages.ToListAsync();
+        }
+
+        public async Task<ActionResult<Garage>> GetByIdAsync(int id)
+        {
+            return await _dbContext.Garages.FirstOrDefaultAsync(p => p.IdMotoConfigurable == id);
+        }
+
+        public async Task AddAsync(Garage entity)
+        {
+            await _dbContext.Garages.AddAsync(entity);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Garage grg, Garage entity)
+        {
+            _dbContext.Entry(grg).State = EntityState.Modified;
+            grg.IdMotoConfigurable = entity.IdMotoConfigurable;
+            grg.IdClient = entity.IdClient;
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(Garage grg)
+        {
+            _dbContext.Garages.Remove(grg);
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
--- SAE_4.01/Program.cs
+++ SAE_4.01/Program.cs
@@ -42,8 +42,10 @@ namespace SAE_4._01
             builder.Services.AddScoped<IDataRepository<EstInclus>, EstInclusManager>();
         #warning faire un IDataRepositoryContenuCommande personnalisé
             builder.Services.AddScoped<IDataRepository<EstLie>, EstLieManager>();
-#warning faire un IDataRepositoryContenuCommande personnalisé
+        #warning faire un IDataRepositoryContenuCommande personnalisé
             builder.Services.AddScoped<IDataRepository<GammeMoto>, GammeMotoManager>();
+            builder.Services.AddScoped<IDataRepository<Garage>, GarageManager>();
+        #warning faire un IDataRepositoryContenuCommande personnalisé
 
             builder.Services.AddScoped<IDataRepository<Prefere>, PrefereManager>();
         #warning faire un IDataRepositoryPresentationEquipement personnalisé => 3 keys
