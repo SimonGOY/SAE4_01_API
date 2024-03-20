68aa55369987bc6fbd12a6e24c3a1db6976f3c01
DatSword <qbastard99@gmail.com>
2024-03-20 16:42:00 +0100

    datamanager estinclus

---
+++ SAE_4.01/Models/DataManager/EstInclusManager.cs
@@ -0,0 +1,50 @@
+﻿using SAE_4._01.Models.EntityFramework;
+using SAE_4._01.Models.Repository;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Runtime.ConstrainedExecution;
+
+namespace SAE_4._01.Models.DataManager
+{
+    public class EstInclusManager : IDataRepository<EstInclus>
+    {
+        readonly BMWDBContext _dbContext;
+
+        public EstInclusManager() { }
+
+        public EstInclusManager(BMWDBContext context)
+        {
+            _dbContext = context;
+        }
+
+        public async Task<ActionResult<IEnumerable<EstInclus>>> GetAllAsync()
+        {
+            return await _dbContext.SontInclus.ToListAsync();
+        }
+
+        public async Task<ActionResult<EstInclus>> GetByIdAsync(int id)
+        {
+            return await _dbContext.SontInclus.FirstOrDefaultAsync(p => p.IdOption == id);
+        }
+
+        public async Task AddAsync(EstInclus entity)
+        {
+            await _dbContext.SontInclus.AddAsync(entity);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(EstInclus ecl, EstInclus entity)
+        {
+            _dbContext.Entry(ecl).State = EntityState.Modified;
+            ecl.IdOption = entity.IdOption;
+            ecl.IdStyle = entity.IdStyle;
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(EstInclus ecl)
+        {
+            _dbContext.SontInclus.Remove(ecl);
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
--- SAE_4.01/Program.cs
+++ SAE_4.01/Program.cs
@@ -39,6 +39,7 @@ namespace SAE_4._01
             builder.Services.AddScoped<IDataRepository<Couleur>, CouleurManager>();
             builder.Services.AddScoped<IDataRepository<DemandeEssai>, DemandeEssaiManager>();
             builder.Services.AddScoped<IDataRepository<Equipement>, EquipementManager>();
+            builder.Services.AddScoped<IDataRepository<EstInclus>, EstInclusManager>();
 
             builder.Services.AddScoped<IDataRepository<Professionnel>, ProfessionnelManager>();
             builder.Services.AddScoped<IDataRepository<Reservation>, ReservationManager>();
