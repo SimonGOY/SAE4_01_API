ef56399cfbc67a9480bcf69f14831ab98a3b85a8
DatSword <qbastard99@gmail.com>
2024-03-29 08:59:45 +0100

    cataccessoire

--- SAE_4.01Tests/Models/DataManager/CategorieAccessoireManagerTests.cs
+++ SAE_4.01Tests/Models/DataManager/CategorieAccessoireManagerTests.cs
@@ -1,12 +1,56 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SAE_4._01.Models.DataManager;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SAE_4._01Tests.Models.DataManager
+namespace SAE_4._01.Models.DataManager.Tests
 {
-    internal class CategorieAccessoireManagerTests
+    [TestClass()]
+    public class CategorieAccessoireManagerTests
     {
+        [TestMethod()]
+        public void CategorieAccessoireManagerTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void CategorieAccessoireManagerTest1()
+        {
+
+        }
+
+        [TestMethod()]
+        public void GetAllAsyncTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void GetByIdAsyncTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void AddAsyncTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void UpdateAsyncTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void DeleteAsyncTest()
+        {
+
+        }
     }
 }
