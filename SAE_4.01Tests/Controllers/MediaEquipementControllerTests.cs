9edf2b0e578cc496f9d1f57426221ea67b8c74b0
DatSword <qbastard99@gmail.com>
2024-04-03 11:43:54 +0200

    debuts test mediaequipements

--- SAE_4.01Tests/Controllers/MediaEquipementControllerTests.cs
+++ SAE_4.01Tests/Controllers/MediaEquipementControllerTests.cs
@@ -13,54 +13,51 @@ using System.Threading.Tasks;
 namespace SAE_4._01.Controllers.Tests
 {
     [TestClass()]
-    public class MediaControllerTests
+    public class MediaEquipementControllerTests
     {
-        private MediaMotoController controller;
+        private MediaEquipementController controller;
         private BMWDBContext context;
-        private IDataRepository<MediaMoto> dataRepository;
-        private MediaMoto medequ;
-        private MediaMoto medmoto;
+        private IDataRepository<MediaEquipement> dataRepository;
+        private MediaEquipement mediaEquipement;
 
         [TestInitialize]
         public void InitTest()
         {
             var builder = new DbContextOptionsBuilder<BMWDBContext>().UseNpgsql("Server=51.83.36.122; port=5432; Database=sa11; uid=sa11; password=BMW-S4; SearchPath=bmw;");
             context = new BMWDBContext(builder.Options);
-            dataRepository = new MediaMotoManager(context);
-            controller = new MediaMotoController(dataRepository);
+            dataRepository = new MediaEquipementManager(context);
+            controller = new MediaEquipementController(dataRepository);
 
-            medequ = new MediaMoto
+            mediaEquipement = new MediaEquipement
             {
-                IdMediaMoto = 666666666,
-                IdMoto = 1,
-                LienMedia = "monimoto.com/media/moto-sportives.jpg",
-
-            };
-
-            medmoto = new MediaMoto
-            {
-                IdMediaMoto = 777777777,
-                IdMoto = 2,
-                LienMedia = "monimoto.com/media/moto-sportives.jpg",
+                IdMediaEquipement = 666666666,
+                IdEquipement = 1,
+                LienMedia = "media.nouvelobs.com/referentiel/633x306/16397249.png",
             };
         }
 
-        /*[TestMethod()]
-        public void GetMediaTest()
+        [TestMethod()]
+        public void GetMediaEquipementsTest_RecuperationsOK()
         {
             //Arrange
-            List<Media> lesMediasCLients = context.Medias.ToList();
+            List<MediaEquipement> lesInfos = context.MediasEquipement.ToList();
             // Act
-            var res = controller.GetMedia().Result;
+            var res = controller.GetMediaEquipements().Result;
             // Assert
             Assert.IsNotNull(res);
-            CollectionAssert.AreEqual(lesMedias, res.Value.ToList(), "Les listes de médias ne sont pas identiques");
-        }*/
+            CollectionAssert.AreEqual(lesInfos, res.Value.ToList(), "Les listes de mediaEqu ne sont pas identiques");
+        }
 
         [TestMethod()]
-        public void GetByIdMotoTest()
+        public void GetMediaTest()
         {
-
+            // Arrange
+            MediaEquipement? inf = context.MediasEquipement.Find(1);
+            // Act
+            var res = controller.GetMedia(1).Result;
+            // Assert
+            Assert.IsNotNull(res.Value);
+            Assert.AreEqual(inf, res.Value, "L'e mediaEqu n'est pas le même");
         }
 
         [TestMethod()]
