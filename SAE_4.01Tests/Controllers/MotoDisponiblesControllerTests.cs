ea6b2b6997194b3e78f0f13226679589e1396a90
DatSword <qbastard99@gmail.com>
2024-04-03 17:25:47 +0200

    tests motodispo option

---
+++ SAE_4.01Tests/Controllers/MotoDisponiblesControllerTests.cs
@@ -0,0 +1,128 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SAE_4._01.Controllers;
+using SAE_4._01.Models.DataManager;
+using SAE_4._01.Models.EntityFramework;
+using SAE_4._01.Models.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAE_4._01.Controllers.Tests
+{
+    [TestClass()]
+    public class MotoDisponiblesControllerTests
+    {
+        private MotoDisponiblesController controller;
+        private BMWDBContext context;
+        private IDataRepository<MotoDisponible> dataRepository;
+        private MotoDisponible motoDispo;
+
+        [TestInitialize]
+        public void InitTest()
+        {
+            var builder = new DbContextOptionsBuilder<BMWDBContext>().UseNpgsql("Server=51.83.36.122; port=5432; Database=sa11; uid=sa11; password=BMW-S4; SearchPath=bmw;");
+            context = new BMWDBContext(builder.Options);
+            dataRepository = new MotoDisponibleManager(context);
+            controller = new MotoDisponiblesController(dataRepository);
+
+            motoDispo = new MotoDisponible
+            {
+                IdMotoDisponible = 666666666,
+                PrixMotoDisponible = 15000.00f,
+                IdMoto = 1,
+            };
+        }
+
+        [TestMethod()]
+        public void GetMotoDisponiblesTest_RecuperationsOK()
+        {
+            //Arrange
+            List<MotoDisponible> lesMots = context.MotoDisponibles.ToList();
+            // Act
+            var res = controller.GetMotoDisponibles().Result;
+            // Assert
+            Assert.IsNotNull(res);
+            CollectionAssert.AreEqual(lesMots, res.Value.ToList(), "Les listes de moto dispo ne sont pas identiques");
+        }
+
+        [TestMethod()]
+        public void GetMotoDisponibleTest_RecuperationOK()
+        {
+            // Arrange
+            MotoDisponible? mot = context.MotoDisponibles.Find(1);
+            // Act
+            var res = controller.GetMotoDisponible(1).Result;
+            // Assert
+            Assert.IsNotNull(res.Value);
+            Assert.AreEqual(mot, res.Value, "La moto dispo n'est pas la même");
+        }
+
+        [TestMethod()]
+        public void GetMotoConfigurableTest_RecuperationFailed()
+        {
+            // Arrange
+            MotoDisponible? med = context.MotoDisponibles.Find(1);
+            // Act
+            var res = controller.GetMotoDisponible(2).Result;
+            // Assert
+            Assert.IsNotNull(res.Value);
+            Assert.AreNotEqual(med, res.Value, "La moto est la même");
+        }
+
+        [TestMethod()]
+        public void GetMotoConfigurableTest_EquipementNExistePas()
+        {
+            var res = controller.GetMotoDisponible(777777777).Result;
+            // Assert
+            Assert.IsNull(res.Result, "La moto existe");
+            Assert.IsNull(res.Value, "La moto existe");
+        }
+
+        [TestMethod()]
+        public void PostDeleteTest()
+        {
+            PostMotoDisponibleTest_CreationOK();
+            PutMotoDisponibleTest_ModificationOK();
+            DeleteMotoDisponibleTest_SuppressionOK();
+        }
+
+        public void PostMotoDisponibleTest_CreationOK()
+        {
+            // Act
+            var result = controller.PostMotoDisponible(motoDispo).Result;
+            // Assert
+            var motRecup = controller.GetMotoDisponible(motoDispo.IdMotoDisponible).Result;
+            motoDispo.IdMotoDisponible = motRecup.Value.IdMotoDisponible;
+            Assert.AreEqual(motoDispo, motRecup.Value, "moto pas identiques");
+        }
+
+        public void PutMotoDisponibleTest_ModificationOK()
+        {
+            // Arrange
+            var motIni = controller.GetMotoDisponible(motoDispo.IdMotoDisponible).Result;
+            motIni.Value.PrixMotoDisponible = 4.50f;
+
+            // Act
+            var res = controller.PutMotoDisponible(motoDispo.IdMotoDisponible, motIni.Value).Result;
+
+            // Assert
+            var motMaj = controller.GetMotoDisponible(motoDispo.IdMotoDisponible).Result;
+            Assert.IsNotNull(motMaj.Value);
+            Assert.AreEqual(motIni.Value, motMaj.Value, "moto pas identiques");
+        }
+
+        public void DeleteMotoDisponibleTest_SuppressionOK()
+        {
+            // Act
+            var motSuppr = controller.GetMotoDisponible(motoDispo.IdMotoDisponible).Result;
+            _ = controller.DeleteMotoDisponible(motoDispo.IdMotoDisponible).Result;
+
+            // Assert
+            var res = controller.GetMotoDisponible(motoDispo.IdMotoDisponible).Result;
+            Assert.IsNull(res.Value, "moto non supprimé");
+        }
+    }
+}
---
+++ SAE_4.01Tests/Controllers/OptionsControllerTests.cs
@@ -0,0 +1,133 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SAE_4._01.Controllers;
+using SAE_4._01.Models.DataManager;
+using SAE_4._01.Models.EntityFramework;
+using SAE_4._01.Models.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAE_4._01.Controllers.Tests
+{
+    [TestClass()]
+    public class OptionsControllerTests
+    {
+        private OptionsController controller;
+        private BMWDBContext context;
+        private IDataRepository<Option> dataRepository;
+        private Option option;
+
+        [TestInitialize]
+        public void InitTest()
+        {
+            var builder = new DbContextOptionsBuilder<BMWDBContext>().UseNpgsql("Server=51.83.36.122; port=5432; Database=sa11; uid=sa11; password=BMW-S4; SearchPath=bmw;");
+            context = new BMWDBContext(builder.Options);
+            dataRepository = new OptionManager(context);
+            controller = new OptionsController(dataRepository);
+
+            option = new Option
+            {
+                IdOption = 666666666,
+                NomOption = "Vidangeage",
+                PrixOption = new decimal (2.00),
+                DetailOption = "Sucaaaaaaaaaaaaage........... d'huile",
+                PhotoOption = "google.com"
+            };
+        }
+
+        [TestMethod()]
+        public void GetOptionsTest_RecuperationsOK()
+        {
+            //Arrange
+            List<Option> lesMots = context.Options.ToList();
+            // Act
+            var res = controller.GetOptions().Result;
+            // Assert
+            Assert.IsNotNull(res);
+            CollectionAssert.AreEqual(lesMots, res.Value.ToList(), "Les listes de moto dispo ne sont pas identiques");
+        }
+
+        [TestMethod()]
+        public void GetOptionTest_RecuperationOK()
+        {
+            // Arrange
+            Option? mot = context.Options.Find(1);
+            // Act
+            var res = controller.GetOption(1).Result;
+            // Assert
+            Assert.IsNotNull(res.Value);
+            Assert.AreEqual(mot, res.Value, "La moto dispo n'est pas la même");
+        }
+
+        [TestMethod()]
+        public void GetMotoConfigurableTest_RecuperationFailed()
+        {
+            // Arrange
+            Option? opt = context.Options.Find(1);
+            // Act
+            var res = controller.GetOption(2).Result;
+            // Assert
+            Assert.IsNotNull(res.Value);
+            Assert.AreNotEqual(opt, res.Value, "La moto est la même");
+        }
+
+        [TestMethod()]
+        public void GetMotoConfigurableTest_EquipementNExistePas()
+        {
+            var res = controller.GetOption(777777777).Result;
+            // Assert
+            Assert.IsNull(res.Result, "La moto existe");
+            Assert.IsNull(res.Value, "La moto existe");
+        }
+
+        [TestMethod()]
+        public void PostDeleteTest()
+        {
+            PostOptionTest_CreationOK();
+            PutOptionTest_ModificationOK();
+            DeleteOptionTest_SuppressionOK();
+        }
+
+
+        public void PostOptionTest_CreationOK()
+        {
+            // Act
+            var result = controller.PostOption(option).Result;
+            // Assert
+            var optRecup = controller.GetOption(option.IdOption).Result;
+            option.IdOption = optRecup.Value.IdOption;
+            Assert.AreEqual(option, optRecup.Value, "option pas identiques");
+        }
+
+
+        public void PutOptionTest_ModificationOK()
+        {
+            // Arrange
+            var optIni = controller.GetOption(option.IdOption).Result;
+            optIni.Value.PrixOption= 10000;
+
+            // Act
+            var res = controller.PutOption(option.IdOption, optIni.Value).Result;
+
+            // Assert
+            var optMaj = controller.GetOption(option.IdOption).Result;
+            Assert.IsNotNull(optMaj.Value);
+            Assert.AreEqual(optIni.Value, optMaj.Value, "moto pas identiques");
+        }
+
+        public void DeleteOptionTest_SuppressionOK()
+        {
+            // Act
+            var optSuppr = controller.GetOption(option.IdOption).Result;
+            _ = controller.DeleteOption(option.IdOption).Result;
+
+            // Assert
+            var res = controller.GetOption(option.IdOption).Result;
+            Assert.IsNull(res.Value, "moto non supprimé");
+        }
+    }
+}
